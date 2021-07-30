d902f49e4b33a7a566773945f673b8cd2143a3f9
Gayatri Kolluru <gkolluru@deloitte.com>
2021-07-30 17:47:17 +0530

    Add files via upload

---
+++ pre-step4-rds/pgdbscripts/customerdb_1.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE customerdb;
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON DATABASE customerdb FROM PUBLIC;
+CREATE ROLE customer_ro;
+GRANT CONNECT ON DATABASE customerdb TO customer_ro;
+CREATE ROLE customer_rw;
+GRANT CONNECT ON DATABASE customerdb TO customer_rw;
+CREATE USER customer_reportuser WITH PASSWORD 'password01';
+GRANT customer_ro TO customer_reportuser;
+CREATE USER customer_appuser WITH PASSWORD 'password02';
+GRANT customer_rw TO customer_appuser;
+\c customerdb
+DROP SCHEMA public;
+\c postgres
+ALTER DATABASE customerdb OWNER TO customer_appuser;
+\c postgresql://customer_appuser:password02@db-ebportal.cqxof8vdhbed.us-east-1.rds.amazonaws.com/customerdb
+CREATE SCHEMA saleslt;
+GRANT USAGE ON SCHEMA saleslt TO customer_ro;
+GRANT USAGE,CREATE ON SCHEMA saleslt TO customer_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA saleslt GRANT SELECT,UPDATE,DELETE,INSERT ON TABLES TO customer_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA saleslt GRANT USAGE ON SEQUENCES TO customer_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA saleslt GRANT SELECT ON TABLES TO customer_ro;
+ALTER DATABASE customerdb SET SEARCH_PATH TO saleslt;
---
+++ pre-step4-rds/pgdbscripts/demodb_1.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE empdb;
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON DATABASE empdb FROM PUBLIC;
+CREATE ROLE emp_ro;
+GRANT CONNECT ON DATABASE empdb TO emp_ro;
+CREATE ROLE emp_rw;
+GRANT CONNECT ON DATABASE empdb TO emp_rw;
+CREATE USER emp_reportuser WITH PASSWORD 'password01';
+GRANT emp_ro TO emp_reportuser;
+CREATE USER emp_appuser WITH PASSWORD 'password02';
+GRANT emp_rw TO emp_appuser;
+\c empdb
+DROP SCHEMA public;
+\c postgres
+ALTER DATABASE empdb OWNER TO emp_appuser;
+\c postgresql://emp_appuser:password02@db-ebportal.cqxof8vdhbed.us-east-1.rds.amazonaws.com/empdb
+CREATE SCHEMA scott;
+GRANT USAGE ON SCHEMA scott TO emp_ro;
+GRANT USAGE,CREATE ON SCHEMA scott TO emp_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA scott GRANT SELECT,UPDATE,DELETE,INSERT ON TABLES TO emp_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA scott GRANT USAGE ON SEQUENCES TO emp_rw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA scott GRANT SELECT ON TABLES TO emp_ro;
+ALTER DATABASE empdb SET SEARCH_PATH TO scott;
